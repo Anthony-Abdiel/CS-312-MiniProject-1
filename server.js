8cb917cbacd4f44a9d10c86cf4dafd69d10d4ded
Anthony Narvaez <aan266@nau.edu>
2025-09-13 12:21:08 -0700

    Finished the skeleton for server.js, added boilerplate on home.ejs

---
+++ server.js
@@ -0,0 +1,29 @@
+//server file
+const express = require("express")
+
+
+//creating the express app
+const app = express()
+PORT = 3000
+
+
+//routes
+
+
+//GET 
+
+app.get("/", async(req, res) => {
+
+    blogPosts
+
+})
+
+//PUT
+
+//POST
+app.post("/post", async(req, res)=>{
+    
+})
+//DELETE
+
+app.listen( PORT, ()=>{console.log("listening on Port: " + PORT)} );
